faa0372cd3edbfec443a7f9f27597508ea677644
Andre Butler <andrebutler@Andres-MacBook-Air.local>
2020-09-29 13:23:58 -0500

    Updates

---
+++ precache-manifest.06d0d1e64d9e4a6c6d44a44a6cf40ddf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed32cac8a44f66582a96097946c0de76",
+    "url": "/PokedexReact/index.html"
+  },
+  {
+    "revision": "a3075a576696f9722b7a",
+    "url": "/PokedexReact/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ea5cfc3202a04704897a",
+    "url": "/PokedexReact/static/js/2.a51ad747.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PokedexReact/static/js/2.a51ad747.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3075a576696f9722b7a",
+    "url": "/PokedexReact/static/js/main.505732a2.chunk.js"
+  },
+  {
+    "revision": "0d150b265eeb45afbee7",
+    "url": "/PokedexReact/static/js/runtime-main.26fd368a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/PokedexReact/static/media/logo.5d5d9eef.svg"
+  }
+]);
